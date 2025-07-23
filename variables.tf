92432f983850963d54ecb0f5873f9b0a9cf27665
Jorge Loaiciga-Rodriguez <jorge.loaicigarodriguez@external.eumetsat.int>
2025-07-23 19:54:26 +0200

    feat: Define user input variables

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "security_group_name" {
+  description = "Name of the security group"
+  type        = string
+  validation {
+    condition     = length(var.security_group_name) > 0
+    error_message = "Security group name must not be empty."
+  }
+}
+
+variable "security_group_description" {
+  description = "Description of the security group"
+  type        = string
+  default     = null
+}
+
+variable "security_group_rules" {
+  description = "List of security group rules"
+  type = list(object({
+    direction        = string
+    ether_type       = string
+    protocol         = string
+    port_range_min   = number
+    port_range_max   = number
+    remote_ip_prefix = string
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "Map of tags to assign to the security group"
+  type        = map(string)
+  default     = {}
+}
+
